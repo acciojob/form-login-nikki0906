24ed3f790583a6063ca505c911adbcef037b4e29
nikki0906 <160705654+nikki0906@users.noreply.github.com>
2024-05-31 14:09:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
+	event.preventDefault(); // Prevent form submission
+
+    // Get the values from the input fields
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+
+    // Display the first and last name using alert
+    alert(firstName + ' ' + lastName);
 
 }
